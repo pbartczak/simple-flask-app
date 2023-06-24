cffb41d8d00837ca01825b461bba8e3ca53e6d0e
Patryk Bartczak <patrykbartczakk@gmail.com>
2023-06-24 15:53:03 +0200

    Add option to define version in .env file

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask, render_template
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def index():
-  return render_template('index.html')
+    return render_template('index.html', app_version=os.getenv('APP_VERSION'))
 
 
 app.run(host='0.0.0.0', port=8080)
